703fd4600a1ba1a5fe2dd3e7c924ab1ebb6554a0
Lucas Lopes <lucas.lopes@kron.digital>
2022-04-19 18:27:49 -0300

    digimon info page development

--- src/components/Pages/Digimon/Digimon.js
+++ src/components/Pages/Digimon/Digimon.js
@@ -1,39 +1,142 @@
-import { Container, Grow, Paper, Typography } from '@mui/material';
+import { Grid, Container, Grow, Paper, Typography, Box, Divider } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { digimonModel } from '../../Models/digimon.model'
 
 const Digimon = () => {
-    const [postData, setPostData] = useState({
-        number: 0,
-        name : '' ,
-        iconSource: '',
-    });
+    const [postData, setPostData] = useState(digimonModel);
     let digimonParams  = useParams();
     const post = useSelector((state) => digimonParams.id ? state.posts.find((p) => p._id === digimonParams.id) : null);
     useEffect(() => {
         if(post) setPostData(post);
     }, [post])
+
     console.log(digimonParams);
 
     return (
         <Container maxwidth="lg" sx={{marginTop: 5}}>
             <Grow in>
-                <Container>
-                        <Paper elevation={3} sx={{
-                            maxWidth: 600,
-                            height: 500,
-                            margin: '0 auto',
-                            overflow: 'auto'
-                        }}>
-                            <Typography variant="h4" component="h2">
-                                {postData.name}
+                <Paper elevation={3} sx={{
+                    maxWidth: 600,
+                    margin: '0 auto',
+                }} style={{
+                    overflowY: 'auto', 
+                    position: 'absolute', 
+                    top: 100, bottom: 10, left: 10, right: 10
+                }}>
+                    <Grid container rowSpacing={3} sx={{padding: 3}}>
+                        <Grid item xs={12} direction="row">
+                            <Typography variant="h4" component="h4" align='center'>
+                                #{postData.number} - {postData.name}
                             </Typography>
-                            <img className="image" loading="lazy" src={postData.iconSource} alt={postData.name}/>
+                        </Grid>
+
+                        <Grid item xs={9} direction="column">
+                            <Typography variant="body2" component="div">
+                                <span>
+                                    Information Information Information Information Information Information Information Information 
+                                </span>
+                            </Typography>
+                        </Grid>
                         
-                        </Paper>
-                    
-                </Container>
+                        <Grid item xs={3} direction="column" style={{textAlign: 'center'}}>
+                            <Box component="span" sx={{ pt: '60px', pb: '40px', pr:'48px', pl: '48px' ,border: '1px dashed grey' }}>
+                                <img className="image" loading="lazy" src={postData.iconSource} alt={postData.name} />
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={12} direction="row">
+                            <Typography variant="body2" component="div">
+                                <b>Level: </b>
+                                <span>{postData.level}</span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Attribute: </b>
+                                <span>{postData.attribute}</span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Type: </b>
+                                <span>
+                                    0
+                                </span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Family: </b>
+                                <span>
+                                    0
+                                </span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Weight: </b>
+                                <span>
+                                    0
+                                </span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Prior forms: </b>
+                                <span>
+                                    0
+                                </span>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                <b>Next forms: </b>
+                                <span>
+                                    0
+                                </span>
+                            </Typography>
+
+                            
+                        </Grid>
+
+                        <Divider />
+
+                        <Grid item xs={12} direction="row">
+                            <Typography variant="h6" component="h6">
+                                <b>Attacks</b>
+                            </Typography>
+                            <ul>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                                <li>
+                                    teste
+                                </li>
+                            </ul>
+                        </Grid>
+
+                        <Grid item xs={12} direction="row">
+                            <Typography variant="h6" component="h6">
+                                <b>Design</b>
+                            </Typography>
+                            <Typography variant="body2" component="div">
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                                Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste 
+                            </Typography>
+                        </Grid>
+                        
+                    </Grid>
+                </Paper>
             </Grow>
         </Container>
     )
